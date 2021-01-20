04377ac895f84f25c625b806b9a937c1bd692cba
nc.devw <nc.devw@gmail.com>
2021-01-20 14:38:50 -0300

    added bulkcreate Users

--- api/index.js
+++ api/index.js
@@ -1,26 +1,33 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 const User = require('./models/User');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
+const utilsUsers = require('./utils/bulkCreate/Users');
 
 const server = new ApolloServer({
-	typeDefs,
-	resolvers,
+    typeDefs,
+    resolvers,
 });
 
 mongoose
-	.connect('mongodb://localhost:27017/quiz', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		autoCreate: true,
-	})
-	.then(async () => {
-		mongoose.connection.db.dropDatabase();
-		console.log('MONGODB CONNECTED');
-		return server.listen({ port: 4000 });
-	})
-	.then((res) => console.log(res.url))
-	.catch(console.log);
+    .connect('mongodb://localhost:27017/quiz', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoCreate: true,
+        useCreateIndex: true,
+    })
+    .then(async() => {
+        mongoose.connection.db.dropDatabase();
+        console.info('MONGODB CONNECTED');
+        try {
+            await User.collection.insertMany(utilsUsers);
+            console.log('BulkCreate Succesful');
+        } catch (err) {
+            console.log('BulkCreate Error', err);
+        }
+        return server.listen({ port: 4000 });
+    })
+    .then((res) => console.log(res.url))
+    .catch(console.log);
---
+++ api/utils/bulkCreate/Users.js
@@ -0,0 +1,39 @@
+const users = [{
+        email: 'nc.devw@gmail.com',
+        password: '!Qwerty123',
+        firstName: 'Nacho',
+        lastName: 'Contreras',
+        countryCode: 1,
+        roleId: 1,
+    },
+    {
+        email: 'emi@mail.com',
+        password: '!Qwerty123',
+    },
+    {
+        email: 'fran@mail.com',
+        password: '!Qwerty123',
+    },
+    {
+        email: 'paco@mail.com',
+        password: '!Qwerty123',
+    },
+    {
+        email: 'tincho@mail.com',
+        password: '!Qwerty123',
+    },
+    {
+        email: 'bruno@mail.com',
+        password: '!Qwerty123',
+    },
+    {
+        email: 'santi@mail.com',
+        password: '!Qwerty123',
+    },
+    {
+        email: 'ailu@mail.com',
+        password: '!Qwerty123',
+    },
+];
+
+module.exports = users;
