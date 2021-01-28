9644496de198bf8e3a02bccd3512657dc4f58e3a
Pakvothe <franqitto@gmail.com>
2021-01-28 17:09:22 -0300

    added messages

--- client/src/components/search_screen/strings.js
+++ client/src/components/search_screen/strings.js
@@ -3,12 +3,16 @@ const strings = {
 		nav: 'Search',
 		title: 'Look for a quiz',
 		ph1: 'Search here...',
+		msg1: 'Your search will appear here...',
+		msg2: 'No quizzes were found... try again!',
 	},
 
 	es: {
 		nav: 'Buscar',
 		title: 'Busca un quiz',
 		ph1: 'Busca aquí...',
+		msg1: 'Tu búsqueda aparecerá aquí...',
+		msg2: 'No se encontró lo que buscaste... ¡Inténtalo de nuevo!',
 	},
 };
 
