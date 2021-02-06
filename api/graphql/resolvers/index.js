88b8825e268d2276a244b0b179ae78aa0b6126b2
nc-devw <nc.devw@gmail.com>
2021-02-06 18:38:41 -0300

    added stats querys

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -1,10 +1,12 @@
 const usersResolvers = require('./users');
 const quizResolvers = require('./quizzes');
+const statsResolvers = require('./stats');
 
 module.exports = {
 	Query: {
 		...usersResolvers.Query,
 		...quizResolvers.Query,
+		...statsResolvers.Query,
 	},
 	Mutation: {
 		...usersResolvers.Mutation,
--- api/graphql/typeDefs/index.js
+++ api/graphql/typeDefs/index.js
@@ -1,8 +1,9 @@
 const userTypes = require('./users');
 const quizzesTypes = require('./quizzes');
+const statsTypes = require('./stats');
 const { gql } = require('apollo-server');
 const { mergeTypeDefs } = require('@graphql-tools/merge');
 
-const types = [userTypes, quizzesTypes];
+const types = [userTypes, quizzesTypes, statsTypes];
 
 module.exports = mergeTypeDefs(types);
